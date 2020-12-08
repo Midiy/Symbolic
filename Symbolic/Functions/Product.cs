a2ca7e29b3739094c231d352afbf31b305f1f9fe
Midiy <midiy009@mail.ru>
2020-12-09 01:13:02 +0300

    Added basic operations and some standart functions.

---
+++ Symbolic/Functions/Product.cs
@@ -0,0 +1,34 @@
+﻿namespace Symbolic.Functions
+{
+    public class Product : Function
+    {
+        public Function Left { get; init; }
+        public Function Right { get; init; }
+
+        public Product(Function left, Function right) => (Left, Right) = (left, right);
+
+        public override Function Diff() => Left.Diff() * Right + Left * Right.Diff();
+
+        public override double GetValue(double variableValue) => Left.GetValue(variableValue) * Right.GetValue(variableValue);
+
+        public override Function Divide(Function other)
+        {
+            if (other == Left) { return Right; }
+            else if (other == Right) { return Left; }
+            else if (other is Product p)
+            {
+                if (p.Left == Left) { return Right.Divide(p.Right); }
+                else if (p.Left == Right) { return Left.Divide(p.Right); }
+                else if (p.Right == Left) { return Right.Divide(p.Left); }
+                else if (p.Right == Right) { return Left.Divide(Left); }
+            }
+            return base.Divide(other);
+        }
+
+        public override Function ApplyTo(Function inner) => Left.ApplyTo(inner) * Right.ApplyTo(inner);
+
+        public override bool Equals(Function? other) => other is Product p && (p.Left == Left && p.Right == Right || p.Left == Right && p.Right == p.Left);
+
+        public override string ToString(string? inner) => $"({Left.ToString(inner)}) * ({Right.ToString(inner)})";
+    }
+}
---
+++ Symbolic/Functions/Standart/Log2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Symbolic.Functions.Standart
+{
+    class Log2 : Function
+    {
+        public override Quotient Diff() => (Quotient)(1 / (new Ln().ApplyTo(2) * Variable!));
+
+        public override double GetValue(double variableValue) => Math.Log2(variableValue);
+
+        public override bool Equals(Function? other) => other is Log2;
+
+        public override string ToString(string? inner) => $"log2({inner})";
+    }
+}
